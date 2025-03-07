80ca489b2a6fbeda3345c1d8428d1f8b8ede1712
manifesto-sauce <167479458+manifesto-sauce@users.noreply.github.com>
2025-03-07 09:08:45 -0600

    Update client.tsx

--- app/about/client.tsx
+++ app/about/client.tsx
@@ -9,10 +9,8 @@ export default function Client() {
         name='hydra'
         className='h-screen w-screen fixed top-0 left-0 -z-10'
         draw={self => {
-          self.s0.initCam();
-      self.src(self.s0).out();
-
-          /*
+self.osc(10, 0.1, 1).out(self.o0);
+self.render(self.o0);          /*
           // Initialize camera for texture input
           self.s0.initCam();
 
