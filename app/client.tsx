1e8cb6c4fb0927e10d9772d99c4924ea8cb2941d
manifesto-sauce <167479458+manifesto-sauce@users.noreply.github.com>
2025-03-07 11:18:22 -0600

    Update client.tsx

--- app/client.tsx
+++ app/client.tsx
@@ -14,34 +14,30 @@ export default function Client() {
       mouseX: number
       mouseY: number
       textStrings: string[]
+      textPositions: { x: number; y: number; word: string }[]
     }
   >
+
   return (
-    <Reactive className='fixed top-0 left-0 h-screen w-screen -z-10'>
-      {
-        // The Processing environment, powered by p5.js (https://p5js.org). Use this component to create the main background image of the homepage.
-      }
+    <Reactive className="fixed top-0 left-0 h-screen w-screen -z-10">
       <Processing
-        name='p'
-        type='p2d'
-        className='h-full w-full absolute top-0 left-0'
-        /**
-         * @props p: the p5 instance, and {props}, the context object consisting of {time, props, elements}. See Reactive section of README.md for more.
-         */
+        name="p"
+        type="p2d"
+        className="h-full w-full absolute top-0 left-0"
         setup={async (p, { props }) => {
-          /**
-           * A callback called whenever the mouse is moved.
-           */
-          p.mouseMoved = ev => {
+          props.mouseX = 0
+          props.mouseY = 0
+          props.textPositions = []
+
+          p.mouseMoved = (ev) => {
             invariant(ev)
-            // Edit the global props object in the context.
-            props.mouseX = ev['clientX']
-            props.mouseY = ev['clientY']
+            props.mouseX = ev.clientX
+            props.mouseY = ev.clientY
           }
-          // This is an asynchronous function because we have to load these images, so we wait for load before calling "res" to finish the function.
-          await new Promise((resolve, reject) => {
+
+          await new Promise((resolve) => {
             let img1: Image, img2: Image
-            // If both images are loaded in, resolve.
+
             const checkCompletion = () => {
               if (img1 && img2) {
                 props.img1 = img1
@@ -49,11 +45,12 @@ export default function Client() {
                 resolve(true)
               }
             }
-            p.loadImage('/img/futurist.png', image => {
+
+            p.loadImage('/img/futurist.png', (image) => {
               img1 = image
               checkCompletion()
             })
-            p.loadImage('/img/manifesto.png', image => {
+            p.loadImage('/img/manifesto.png', (image) => {
               img2 = image
               checkCompletion()
             })
@@ -68,14 +65,12 @@ export default function Client() {
               'Sound Artist',
               'Some Guy',
               'Media Artist',
- 
             ]
 
             p.textFont('Andale Mono')
           })
         }}
         draw={(p, { props, time }: Context) => {
-          // Don't draw until setup is completed
           if (!props.img1 || !props.img2 || !props.textStrings) return
           p.clear()
           p.fill('lightgreen')
@@ -83,18 +78,22 @@ export default function Client() {
 
           let i = 0
           const t = time * 100
-          // Draw each of the textStrings in the setup function
+          props.textPositions = []
+
           for (let string of props.textStrings) {
             i++
-            p.textSize(10 + (p.sin(i * 2.5249 + time) * 0.5 + 0.5) * 48)
-            p.text(
-              string,
-              (i * t) % (p.width / 2),
-              (i * t + 0.5 * 100) % (p.height / 2)
-            )
+            const textSize =
+              10 + (p.sin(i * 2.5249 + time) * 0.5 + 0.5) * 48
+            p.textSize(textSize)
+
+            const xPos = (i * t) % (p.width / 2)
+            const yPos = (i * t + 0.5 * 100) % (p.height / 2)
+
+            props.textPositions.push({ x: xPos, y: yPos, word: string })
+
+            p.text(string, xPos, yPos)
           }
 
-          // Draw the two album covers, moving around in response to mouse movement.
           p.image(
             props.img1,
             (props.mouseX + (((time / 3) * p.width) % (p.width / 2))) %
@@ -110,36 +109,44 @@ export default function Client() {
             100,
             100
           )
-        }}
-      />
-      {
-        // A top layer of the background, that just has a little shader to draw horizontal lines. More about GLSL fragment shaders: https://webgl2fundamentals.org/webgl/lessons/webgl-shaders-and-glsl.html
-        // The filter shader is passed a vertex shader with that creates the vTexCoord parameter.
-      }
-      <Processing
-        type='webgl'
-        name='overlay'
-        className='absolute top-0 left-0 h-full w-full'
-        draw={(p, { time }) => {
-          // @ts-ignore
-          const shader = p.createFilterShader(/*glsl*/ `
-            precision highp float;
-            varying vec2 vTexCoord;
-            uniform float time;
-            uniform float height;
-            void main() {
-              if (mod(vTexCoord.y * height, 5.) < 2.) {
-                gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
-              } else {
-                gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);
-              }
-            }`)
 
-          shader.setUniform('time', time)
-          shader.setUniform('height', p.height)
-          p.filter(shader)
+          // Check for text hover
+          const hoverThreshold = 30
+          for (let { x, y, word } of props.textPositions) {
+            if (
+              Math.abs(props.mouseX - x) < hoverThreshold &&
+              Math.abs(props.mouseY - y) < hoverThreshold
+            ) {
+              playSound(word)
+            }
+          }
         }}
       />
     </Reactive>
   )
+}
+
+const wordAudioMap = {
+  Composer: '/sounds/composer.mp3',
+  Performer: '/sounds/performer.mp3',
+  Teacher: '/sounds/teacher.mp3',
+  'Music man': '/sounds/musicman.mp3',
+  Improviser: '/sounds/improviser.mp3',
+  Programmer: '/sounds/programmer.mp3',
+  'Sound Artist': '/sounds/soundartist.mp3',
+  'Some Guy': '/sounds/someguy.mp3',
+  'Media Artist': '/sounds/mediaartist.mp3',
+}
+
+const audioCache = {}
+
+function playSound(word) {
+  if (!wordAudioMap[word]) return
+
+  if (!audioCache[word]) {
+    audioCache[word] = new Audio(wordAudioMap[word])
+  }
+
+  audioCache[word].currentTime = 0
+  audioCache[word].play()
 }
