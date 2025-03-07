9d769b27374d282e2868f13f1c9607b44b7ca9a4
manifesto-sauce <167479458+manifesto-sauce@users.noreply.github.com>
2025-03-07 10:46:52 -0600

    Update client.tsx

--- app/work/client.tsx
+++ app/work/client.tsx
@@ -9,7 +9,20 @@ export default function Client() {
         name='h'
         className='fixed top-0 left-0 h-screen w-screen -z-10'
         draw={self => {
-          self.osc(10, 0.4).out()
+  self.src(self.o0).modulate(
+  
+  self.noise(3)
+  ,  0.2)
+  .blend(
+  
+self.shape(3, 0.1, 0.1).rotate(-1.75).scroll(
+  ()=>0.4-mouse.x/window.innerWidth,
+  ()=>0.4-mouse.y/window.innerHeight)
+  
+  , 0.4).out(self.o0)
+
+
+          
         }}
       />
     </Reactive>
