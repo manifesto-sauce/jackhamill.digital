cd4d49ee208271153cb4d1553752b25d4cf5ee2c
manifesto-sauce <167479458+manifesto-sauce@users.noreply.github.com>
2025-03-08 12:01:03 -0600

    Update client.tsx

--- app/news/client.tsx
+++ app/news/client.tsx
@@ -1,20 +1,22 @@
-'use client'
-import { Hydra, Reactive } from 'reactive-frames'
+'use client';
+import { Hydra, Reactive } from 'reactive-frames';
 
 export default function Client() {
   return (
-    <Reactive className='fixed top-0 left-0 h-screen w-screen -z-10'>
+    <Reactive className="fixed top-0 left-0 h-screen w-screen -z-10">
       <Hydra
-        name='h'
-        className='fixed top-0 left-0 h-screen w-screen -z-10'
-        draw={h => {
-  self.osc(2,.1).modulate(noise(3),.4).diff(self.o0)
-  	.modulateScrollX(self.osc(0.2).modulate(self.osc().rotate(),.2))
-	.scale(0.5).color(0.3,0.014,01)
-  	.out()
-
+        name="h"
+        className="fixed top-0 left-0 h-screen w-screen -z-10"
+        draw={(h) => {
+          self.osc(2, 0.1)
+            .modulate(self.noise(3), 0.4)
+            .diff(self.o0)
+            .modulateScrollX(self.osc(0.2).modulate(self.osc().rotate(), 0.2))
+            .scale(0.5)
+            .color(0.3, 0.014, 1)
+            .out();
         }}
       />
     </Reactive>
-  )
+  );
 }
