b30546df14530a38ba330bad5c170c11b8667680
manifesto-sauce <167479458+manifesto-sauce@users.noreply.github.com>
2025-03-08 11:59:54 -0600

    Update client.tsx

--- app/news/client.tsx
+++ app/news/client.tsx
@@ -8,20 +8,11 @@ export default function Client() {
         name='h'
         className='fixed top-0 left-0 h-screen w-screen -z-10'
         draw={h => {
-          const { o0, o1, o2, o3 } = h
-          h.noise(3, 0.3, 3).thresh(0.3, 0.03).diff(o3, 0.3).out(o1)
-          h.gradient([0.3, 0.3, 3]).diff(o0).blend(o1).out(o3)
-          h.voronoi(33, 3, 30)
-            .rotate(3, 0.3, 0)
-            .modulateScale(o2, 0.3)
-            .color(-3, 3, 0)
-            .brightness(3)
-            .out(o0)
-          h.shape(30, 0.3, 1)
-            .invert(({ time }) => Math.sin(time) * 3)
-            .out(o2)
+  self.osc(2,.1).modulate(noise(3),.4).diff(self.o0)
+  	.modulateScrollX(self.osc(0.2).modulate(self.osc().rotate(),.2))
+	.scale(0.5).color(0.3,0.014,01)
+  	.out()
 
-          h.render(o3)
         }}
       />
     </Reactive>
