62752aec94dc7453a8ded858c73377736f3e238b
manifesto-sauce <167479458+manifesto-sauce@users.noreply.github.com>
2025-03-09 15:59:50 -0500

    Update works.tsx

--- app/work/works.tsx
+++ app/work/works.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import Section from '@/components/Section'
 import { useSelectedLayoutSegment } from 'next/navigation'
 import { ProjectsQueryResult } from '../../sanity/sanity-types'
 import LinkFrame from '@/components/LinkFrame'
@@ -8,25 +7,30 @@ import LinkFrame from '@/components/LinkFrame'
 export default function Works({ projects }: { projects: ProjectsQueryResult }) {
   const service = useSelectedLayoutSegment()
 
+  // Filter projects based on the selected service
+  const filteredProjects = projects.filter(project => project.category === service)
+
   return (
     <>
-      {/* LinkFrame is the container for works, customize the classes to change things */}
-      {projects
-        .filter(
-         project => !service || 
-            project.category === service
-        )
-        .map(project => (
-          <LinkFrame
-            key={project._id}
-            href={`/work/${project.category}/${project.slug}`}
-            title={project.title}
-            subtitle={project.subtitle}
-            banner={project.banner}
-        className='w-2/3 md:w-3/8 lg:w-1/4 aspect-square p-4'
-            innerClassName='border border-accent w-full aspect-square p-4 rounded hover:bg-accent/30 transition-colors duration-300 relative bg-black/50 backdrop-blur-lg'
-          />
-        ))}
+      {filteredProjects.length > 0 ? (
+        <div className="flex flex-wrap gap-4 justify-center">
+          {filteredProjects.map(project => (
+            <LinkFrame
+              key={project._id}
+              href={`/work/${project.category}/${project.slug}`}
+              title={project.title}
+              subtitle={project.subtitle}
+              banner={project.banner}
+              className="w-2/3 md:w-3/8 lg:w-1/4 aspect-square p-4"
+              innerClassName="border border-accent w-full aspect-square p-4 rounded hover:bg-accent/30 transition-colors duration-300 relative bg-black/50 backdrop-blur-lg"
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="flex items-center justify-center min-h-[50vh] [mt-50vh]">
+          <p className="text-lg text-center text-gray-400">Sort by category on the left. "Compositions" refers to pieces where I was not the sole performer. "Performances" includes all projects where I was the solo performer. "Fixed media" refers to work without performers. </p>
+        </div>
+      )}
     </>
   )
 }
