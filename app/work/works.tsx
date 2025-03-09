32b3f7427f6b5576f6dc58a1ead8a2cf12cb6ba6
manifesto-sauce <167479458+manifesto-sauce@users.noreply.github.com>
2025-03-09 15:20:23 -0500

    Update works.tsx

--- app/work/works.tsx
+++ app/work/works.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import Section from '@/components/Section'
 import { useSelectedLayoutSegment } from 'next/navigation'
 import { ProjectsQueryResult } from '../../sanity/sanity-types'
 import LinkFrame from '@/components/LinkFrame'
@@ -12,22 +11,26 @@ export default function Works({ projects }: { projects: ProjectsQueryResult }) {
   const filteredProjects = projects.filter(project => project.category === service)
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-[50vh]">
+    <>
       {filteredProjects.length > 0 ? (
-        filteredProjects.map(project => (
-          <LinkFrame
-            key={project._id}
-            href={`/work/${project.category}/${project.slug}`}
-            title={project.title}
-            subtitle={project.subtitle}
-            banner={project.banner}
-            className="w-2/3 md:w-3/8 lg:w-1/4 aspect-square p-4"
-            innerClassName="border border-accent w-full aspect-square p-4 rounded hover:bg-accent/30 transition-colors duration-300 relative bg-black/50 backdrop-blur-lg"
-          />
-        ))
+        <div className="flex flex-wrap gap-4 justify-center">
+          {filteredProjects.map(project => (
+            <LinkFrame
+              key={project._id}
+              href={`/work/${project.category}/${project.slug}`}
+              title={project.title}
+              subtitle={project.subtitle}
+              banner={project.banner}
+              className="w-2/3 md:w-3/8 lg:w-1/4 aspect-square p-4"
+              innerClassName="border border-accent w-full aspect-square p-4 rounded hover:bg-accent/30 transition-colors duration-300 relative bg-black/50 backdrop-blur-lg"
+            />
+          ))}
+        </div>
       ) : (
-        <p className="text-lg text-center text-gray-400">No projects found for this category.</p>
+        <div className="flex items-center justify-center min-h-[50vh]">
+          <p className="text-lg text-center text-gray-400">No projects found for this category.</p>
+        </div>
       )}
-    </div>
+    </>
   )
 }
