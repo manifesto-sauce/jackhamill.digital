ab7af9ca7ec409b9c52d8920bea628a6d7e3c338
manifesto-sauce <167479458+manifesto-sauce@users.noreply.github.com>
2025-03-09 15:40:00 -0500

    Update works.tsx

--- app/work/works.tsx
+++ app/work/works.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Section from '@/components/Section'
 import { useSelectedLayoutSegment } from 'next/navigation'
 import { ProjectsQueryResult } from '../../sanity/sanity-types'
 import LinkFrame from '@/components/LinkFrame'
@@ -7,30 +8,26 @@ import LinkFrame from '@/components/LinkFrame'
 export default function Works({ projects }: { projects: ProjectsQueryResult }) {
   const service = useSelectedLayoutSegment()
 
-  // Filter projects based on the selected service
-  const filteredProjects = projects.filter(project => project.category === service)
-
   return (
     <>
-      {filteredProjects.length > 0 ? (
-        <div className="flex flex-wrap gap-4 justify-center">
-          {filteredProjects.map(project => (
-            <LinkFrame
-              key={project._id}
-              href={`/work/${project.category}/${project.slug}`}
-              title={project.title}
-              subtitle={project.subtitle}
-              banner={project.banner}
-              className="w-2/3 md:w-3/8 lg:w-1/4 aspect-square p-4"
-              innerClassName="border border-accent w-full aspect-square p-4 rounded hover:bg-accent/30 transition-colors duration-300 relative bg-black/50 backdrop-blur-lg"
-            />
-          ))}
-        </div>
-      ) : (
-        <div className="flex items-center justify-center min-h-[50vh]">
-          <p className="text-lg text-center text-gray-400">Sort by category on the left. "Compositions" are scored works, where I was not the only performer. "Performances" are improvisations and compositions where I was the performer. </p>
-        </div>
-      )}
+      {/* LinkFrame is the container for works, customize the classes to change things */}
+      {projects
+        .filter(
+          project => 
+          !service || 
+            project.category === service
+        )
+        .map(project => (
+          <LinkFrame
+            key={project._id}
+            href={/work/${project.category}/${project.slug}}
+            title={project.title}
+            subtitle={project.subtitle}
+            banner={project.banner}
+        className='w-2/3 md:w-3/8 lg:w-1/4 aspect-square p-4'
+            innerClassName='border border-accent w-full aspect-square p-4 rounded hover:bg-accent/30 transition-colors duration-300 relative bg-black/50 backdrop-blur-lg'
+          />
+        ))}
     </>
   )
-}
+} 
